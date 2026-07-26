3b2b343de79263e111d36f8bafdfc249105ee402
ulysse <ujebbari@gmail.com>
2026-07-26 17:24:18 +0900

    Fix nativeKeyEvent import

--- app/src/main/java/com/example/kanjiquiz/MainActivity.kt
+++ app/src/main/java/com/example/kanjiquiz/MainActivity.kt
@@ -76,7 +76,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.input.key.Key
 import androidx.compose.ui.input.key.KeyEventType
 import androidx.compose.ui.input.key.key
-import androidx.compose.ui.input.key.nativeKeyEvent
 import androidx.compose.ui.input.key.onPreviewKeyEvent
 import androidx.compose.ui.input.key.type
 import androidx.compose.ui.graphics.Color
